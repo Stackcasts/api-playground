b2b592bb78d85b67655c6eecf85488f46f47255c
Nathan Pope <>
2020-04-18 12:37:21 -0700

    Moved the usage documentation to the README.md and added documentation for Updating a user and deleting a user

--- app/Http/Controllers/DocumentationController.php
+++ app/Http/Controllers/DocumentationController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 class DocumentationController extends Controller
 {
     public function index() {
-        $path = base_path('docs/instructions.md');
+        $path = base_path('README.md');
 
         $contents = file_get_contents($path);
 
